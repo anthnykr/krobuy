c6e24bf62126339180e55883276a2b8f960f9709
Anthony Kroeger <anthony.kroe@gmail.com>
2023-03-06 08:20:24 +1100

    subtotal fixed in order summary

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -10,33 +10,29 @@ const Cart: NextPage = () => {
   // Getting cart from local storage whenever cart gets changed
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart") || "[]"));
-  }, [cart]);
+  }, []);
 
   const { products, error } = productData();
   if (error) return <div>Failed to load</div>;
   if (!products) return <div>Loading...</div>;
 
+  let subtotal = 0;
+
   return (
     <PageLayout pageTitle="Cart">
       <main className="flex flex-col lg:flex-row">
         {/* TODO: play with the width especially small screens */}
-        <section className="w-[600px] space-y-3 p-6">
-          <h1 className="text-2xl font-bold text-blue-600">Cart</h1>
+        <section className="w-[600px] p-6">
+          <h1 className="mb-6 text-2xl font-bold text-blue-600">Cart</h1>
           {cart &&
             cart.map((item, index) => {
               return (
-                <>
-                  <div className="flex items-center gap-6" key={index}>
+                <div key={item.productName}>
+                  <div className="flex items-center gap-6">
                     <div className="relative h-[150px] w-[150px]">
-                      {/* TODO: fix this page when Context API is implemented */}
                       {/* TODO: make the cart item into a component */}
                       <Image
-                        src={
-                          products.filter(
-                            (product: any) =>
-                              product.productName === item.productName
-                          )[0].productImage
-                        }
+                        src={item.productImage}
                         alt={`${item.productName} Image`}
                         fill
                       />
@@ -47,30 +43,20 @@ const Cart: NextPage = () => {
                         {item.productName}
                       </h1>
                       <p className="text-gray-700">
-                        Unit Price: $
-                        {
-                          products.filter(
-                            (product: any) =>
-                              product.productName === item.productName
-                          )[0].productPrice
-                        }
+                        Unit Price: ${item.productPrice}
                       </p>
                       <p className="text-gray-700">
                         <span>Quantity: </span>
                         {item.quantity}
                       </p>
                       <p className="text-gray-700">
-                        Subtotal: $
-                        {products.filter(
-                          (product: any) =>
-                            product.productName === item.productName
-                        )[0].productPrice * item.quantity}
+                        Subtotal: ${item.quantity * item.productPrice}
                       </p>
                     </div>
                   </div>
 
-                  {index !== cart.length - 1 && <hr />}
-                </>
+                  {index !== cart.length - 1 && <hr className="my-6" />}
+                </div>
               );
             })}
         </section>
@@ -82,7 +68,14 @@ const Cart: NextPage = () => {
           <div className="flex flex-col gap-1 rounded-lg bg-gray-300 p-6 text-gray-600">
             <p className="flex justify-between">
               <span>Subtotal</span>
-              <span>$0.00</span>
+              <span>
+                $
+                {cart.reduce(
+                  (sum, currentValue) =>
+                    sum + currentValue.productPrice * currentValue.quantity,
+                  0
+                )}
+              </span>
             </p>
 
             <p className="flex justify-between">
